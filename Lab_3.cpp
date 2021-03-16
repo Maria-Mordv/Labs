981502953f7221af23f930ad3a8fd422c4f5b239
Maria-Mordv <79911298+Maria-Mordv@users.noreply.github.com>
2021-03-16 13:37:33 +0600

    Add files via upload

---
+++ Lab_3.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+int CorrectNumber(string x) {
+    int n = 0;
+    while (true)
+    {
+        cout << x;
+        cin >> n;
+        if (!cin || n <= 0)
+        {
+            cout << "Ошибка ввода!\n";
+            cin.clear();
+            while (cin.get() != '\n');
+        }
+        else break;
+    }
+    return n;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int size = CorrectNumber("Введите целое положительно число ");
+    int* array = new int[size];
+    int min = 9999;
+
+    srand(time(0));
+    for (int i = 0; i < size; i++) {
+        array[i] = rand() % 1000 - 500;
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << "\n";
+
+    for (int i = 0; i < size; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    cout << "минимальное значение: " << min;
+    return 0;
+}
