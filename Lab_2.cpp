255a695690aab71b6736ff72f55f13e45b2595c3
Maria-Mordv <79911298+Maria-Mordv@users.noreply.github.com>
2021-03-16 11:41:47 +0600

    Add files via upload

---
+++ Lab_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int CorrectNumber(string x) {
+    int n = 0;
+    while (true)
+    {
+        cout << x;
+        cin >> n;
+        if (!cin || n < 0)
+        {
+            cout << "Ошибка ввода!\n";
+            cin.clear();
+            while (cin.get() != '\n');
+        }
+        else break;
+    }
+    return n;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n = CorrectNumber("Введите целое положительное число ");
+    int x = 1;
+    
+    for (int i = 1; i <= n; i++) {
+        x = i * x;
+    }
+    cout << x;
+    return 0;
+}
